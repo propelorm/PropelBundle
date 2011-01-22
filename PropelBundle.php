e9e476a784da4af26bb90ae31c06c824b1186967
William DURAND <william.durand1@gmail.com>
2011-01-22 16:15:43 +0100

    Refactored bundle class

--- PropelBundle.php
+++ PropelBundle.php
@@ -3,6 +3,7 @@
 namespace Symfony\Bundle\PropelBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class PropelBundle extends Bundle
 {
@@ -13,7 +14,28 @@ class PropelBundle extends Bundle
         if (0 === strncasecmp(PHP_SAPI, 'cli', 3)) {
             set_include_path($this->container->getParameter('propel.phing_path').'/classes'.PATH_SEPARATOR.get_include_path());
         }
+    }
 
-//        $this->container->getPropelService();
+    public function registerExtensions(ContainerBuilder $container)
+    {
+        $this->initReflection();
+
+        parent::registerExtensions($container);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getNamespace()
+    {
+        return __NAMESPACE__;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getPath()
+    {
+        return strtr(__DIR__, '\\', '/');
     }
 }
