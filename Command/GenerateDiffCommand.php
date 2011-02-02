d4d981a25c6252fb2f5477be04f3ab77af6b17a8
William DURAND <william.durand1@gmail.com>
2011-02-02 02:06:38 +0100

    Added a new command : propel:generate-diff

---
+++ Command/GenerateDiffCommand.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Symfony\Bundle\PropelBundle\Command;
+
+use Symfony\Bundle\PropelBundle\Command\PhingCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/*
+ * This file is part of the Symfony framework.
+ *
+ * (c) Fabien Potencier <fabien.potencier@symfony-project.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+/**
+ * GenerateDiffCommand.
+ *
+ * @author William DURAND <william.durand1@gmail.com>  
+ */
+class GenerateDiffCommand extends PhingCommand
+{
+    /**
+     * @see Command
+     */
+    protected function configure()
+    {
+        $this
+            ->setDescription('Generates SQL diff between the XML schemas and the current database structure')
+            ->setHelp(<<<EOT
+The <info>propel:generate-diff</info> command compares the current database structure and the available schemas. If there is a difference, it creates a migration file.
+
+  <info>php app/console propel:generate-diff</info>
+EOT
+            )
+            ->setName('propel:generate-diff')
+        ;
+    }
+
+    /**
+     * @see Command
+     *
+     * @throws \InvalidArgumentException When the target directory does not exist
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {   
+        $this->callPhing('diff');
+    }
+}
