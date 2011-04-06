a6e6d5f05465b52d4f260e9936a1324f4bafd8c7
William DURAND <william.durand1@gmail.com>
2011-04-07 01:53:23 +0200

    Fixed default value for the 'password' parameter in the configuration tree

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -132,7 +132,7 @@ class Configuration implements ConfigurationInterface
                 ->children()
                 ->scalarNode('driver')->defaultValue('mysql')->end()
                 ->scalarNode('user')->defaultValue('root')->end()
-                ->scalarNode('password')->defaultNull()->end()
+                ->scalarNode('password')->defaultValue('')->end()
                 ->scalarNode('dsn')->defaultValue('')->end()
                 ->scalarNode('classname')->defaultValue($this->debug ? 'DebugPDO' : 'PropelPDO')->end()
             ->end()
