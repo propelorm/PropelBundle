e181e2d9952e7b8e6674b546eeb578feeee952a8
William DURAND <william.durand1@gmail.com>
2011-04-19 14:16:28 +0200

    Minor changes on graphviz command

--- Command/GraphvizCommand.php
+++ Command/GraphvizCommand.php
@@ -7,19 +7,10 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-/*
- * This file is part of the Symfony framework.
- *
- * (c) Fabien Potencier <fabien.potencier@symfony-project.com>
- *
- * This source file is subject to the MIT license that is bundled
- * with this source code in the file LICENSE.
- */
-
 /**
  * GraphvizCommand.
  *
- * @author William DURAND <william.durand1@gmail.com>  
+ * @author William DURAND <william.durand1@gmail.com>
  */
 class GraphvizCommand extends PhingCommand
 {
@@ -32,7 +23,7 @@ class GraphvizCommand extends PhingCommand
             ->setDescription('Generates Graphviz file for your project')
             ->setHelp(<<<EOT
 The <info>propel:graphviz</info> generates Graphviz file for your project.
-          
+
   <info>php app/console propel:graphviz</info>
 EOT
             )
@@ -46,7 +37,7 @@ EOT
      * @throws \InvalidArgumentException When the target directory does not exist
      */
     protected function execute(InputInterface $input, OutputInterface $output)
-    {  
+    {
         $dest = $this->getApplication()->getKernel()->getRootDir() . '/propel/graph/';
 
         $this->callPhing('graphviz', array(
