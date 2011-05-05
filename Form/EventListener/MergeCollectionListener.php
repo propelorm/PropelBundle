0bd6875f20cb1f3d8cca3e1310688fb650496e65
William DURAND <william.durand1@gmail.com>
2011-05-05 19:17:40 +0200

    Fixed the way the listener compares two objects

--- Form/EventListener/MergeCollectionListener.php
+++ Form/EventListener/MergeCollectionListener.php
@@ -25,7 +25,7 @@ class MergeCollectionListener implements EventSubscriberInterface
         } else {
             // merge $data into $collection
             foreach ($collection as $model) {
-                if (!$data->contains($model)) {
+                if ($data->search($model) === false) {
                     $collection->remove($model);
                 } else {
                     $data->remove($model);
