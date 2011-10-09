b69d89497934859b2c6be926be004daa9bb6db60
William DURAND <william.durand1@gmail.com>
2011-10-09 20:12:58 +0300

    Fixed missing exception

--- Security/User/ModelUserProvider.php
+++ Security/User/ModelUserProvider.php
@@ -13,7 +13,7 @@ namespace Propel\PropelBundle\Security\User;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
-use Symfony\Component\Security\Core\Exception\UnsupportedAccountException;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 
 /**
  * ModelUserProvider class.
@@ -83,7 +83,7 @@ class ModelUserProvider implements UserProviderInterface
     public function refreshUser(UserInterface $user)
     {
         if ($user instanceof $this->class) {
-            throw new UnsupportedAccountException(sprintf('Instances of "%s" are not supported.', get_class($user)));
+            throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', get_class($user)));
         }
 
         return $this->loadUserByUsername($user->getUsername());
