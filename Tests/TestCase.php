b644e3473895a3f441364f41768d111a43d23a48
William DURAND <william.durand1@gmail.com>
2011-12-14 14:49:39 +0100

    Fixed typo

--- Tests/TestCase.php
+++ Tests/TestCase.php
@@ -40,7 +40,7 @@ class TestCase extends \PHPUnit_Framework_TestCase
     {
         require_once __DIR__.'/../vendor/propel/runtime/lib/Propel.php';
         require_once __DIR__.'/../vendor/propel/runtime/lib/adapter/DBAdapter.php';
-        require_once __DIR__.'/../vendor/propel/runtime/lib/adapter/DBSqlite.php';
+        require_once __DIR__.'/../vendor/propel/runtime/lib/adapter/DBSQLite.php';
         require_once __DIR__.'/../vendor/propel/runtime/lib/connection/PropelPDO.php';
         require_once __DIR__.'/../vendor/propel/generator/lib/util/PropelQuickBuilder.php';
     }
