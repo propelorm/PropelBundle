9c4ad7db458aceaeb2d4ff8f3c3665c7d759d2c3
Marc J. Schmidt <marc@marcjschmidt.de>
2016-04-13 19:01:57 +0200

    Fixed yaml dumper test with newer versions

--- Tests/DataFixtures/Dumper/YamlDataDumperTest.php
+++ Tests/DataFixtures/Dumper/YamlDataDumperTest.php
@@ -56,6 +56,12 @@ class YamlDataDumperTest extends TestCase
 YAML;
 
         $result = file_get_contents($filename);
+
+        //yaml changed the way objects are serialized in
+        // -> https://github.com/symfony/yaml/commit/d5a7902da7e5af069bb8fdcfcf029a229deb1111
+        //so we need to replace old behavior with new, to get this test working in all versions
+        $result = str_replace(' !!php/object', ' !php/object', $result);
+
         $this->assertEquals($expected, $result);
     }
 }
